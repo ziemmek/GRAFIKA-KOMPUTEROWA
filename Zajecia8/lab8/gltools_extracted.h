ab7dee7f3b7fdf104332c5e5964efd3399588fdd
Ziemowit Juroszek <ziemmek@users.noreply.github.com>
2018-06-13 00:28:23 +0200

    Add files via upload

---
+++ Zajecia8/lab8/gltools_extracted.h
@@ -0,0 +1,7 @@
+#include <GL/glut.h>
+
+// Some data types
+typedef GLfloat GLTVector2[2];      // Two component floating point vector
+typedef GLfloat GLTVector3[3];      // Three component floating point vector
+typedef GLfloat GLTVector4[4];      // Four component floating point vector
+typedef GLfloat GLTMatrix[16];      // A column major 4x4 matrix of type GLfloat
